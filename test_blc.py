2cb59c4c4e28e752549c9a99ed8a2fcbd7b75eb5
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2016-02-15 12:34:23 +0100

    Added dummy test file

---
+++ test_blc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import unittest
+
+class TestFake(unittest.TestCase):
+  def test_upper(self):
+    self.assertEqual('foo'.upper(), 'FOO')
+    
+
+if __name__ == '__main__':
+  unittest.main()
+  
+        
